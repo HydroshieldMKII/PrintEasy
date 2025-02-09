080161f06baab504065bd25f118208ef03249973
HydroshieldMKII <v.bureau@hotmail.com>
2025-02-08 23:04:01 -0500

    Enhance login component with PrimeNG UI elements and form validation

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+
+import { PrimeNG } from 'primeng/config';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +10,11 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Angular';
+export class AppComponent implements OnInit {
+  title = 'app';
+
+  constructor(private primeng: PrimeNG) { }
+  ngOnInit() {
+    this.primeng.ripple.set(true);
+  }
 }
--- Angular/src/app/login/login.component.ts
+++ Angular/src/app/login/login.component.ts
@@ -1,11 +1,72 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+// PrimeNG
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { CheckboxModule } from 'primeng/checkbox';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { SliderModule } from 'primeng/slider';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { RatingModule } from 'primeng/rating';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { FileUploadModule } from 'primeng/fileupload';
+import { TableModule } from 'primeng/table';
+import { DatePicker } from 'primeng/datepicker';
+import { Select } from 'primeng/select';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CardModule,
+    ButtonModule,
+    InputTextModule,
+    CalendarModule,
+    DropdownModule,
+    CheckboxModule,
+    RadioButtonModule,
+    SliderModule,
+    ToggleButtonModule,
+    RatingModule,
+    ProgressBarModule,
+    FileUploadModule,
+    TableModule,
+    ProgressSpinnerModule,
+    DatePicker,
+    Select
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  submitted = false;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.loginForm.valid) {
+      console.log("Login successful!", this.loginForm.value);
+    }
+  }
 }
