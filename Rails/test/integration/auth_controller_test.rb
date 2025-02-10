7ca2bffd2301142def531960d84239e80c120e0d
Venorrak <jaudet199@gmail.com>
2025-02-10 14:25:12 -0500

    finished all tests

--- Rails/test/integration/auth_controller_test.rb
+++ Rails/test/integration/auth_controller_test.rb
@@ -8,7 +8,7 @@ class AuthControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should sign up" do
-    post users_sign_up_path, params: { user: { username: "test", password: "password", password_confirmation: "password" } }
+    post user_registration_path, params: { user: { username: "test", password: "password", password_confirmation: "password" } }
 
     assert_response :success
     assert_nothing_raised do
@@ -37,14 +37,149 @@ class AuthControllerTest < ActionDispatch::IntegrationTest
 
   # ------------ DOES NOT WORK ------------
   
+  # Sign up
   test "should not sign up -> no user" do
-    post users_sign_up_path, params: {}
+    post user_registration_path, params: {}
 
-    assert_response :bad_request
-    p response.body
+    assert_response :unprocessable_entity
     assert_nothing_raised do
       @parsed_response = JSON.parse(response.body)
     end
-    p @parsed_response
+    
+    assert_equal ["param is missing or the value is empty: user"], @parsed_response["errors"]["user"]
+  end
+
+  test "should not sign up -> no username" do
+    post user_registration_path, params: { user: { password: "password", password_confirmation: "password" } }
+
+    assert_response :unprocessable_entity
+    assert_nothing_raised do
+      @parsed_response = JSON.parse(response.body)
+    end
+    assert_equal ["can't be blank"], @parsed_response["errors"]["username"]
+  end
+
+  test "should not sign up -> no password" do
+    post user_registration_path, params: { user: { username: "test", password_confirmation: "password" } }
+
+    assert_response :unprocessable_entity
+    assert_nothing_raised do
+      @parsed_response = JSON.parse(response.body)
+    end
+    assert_equal ["can't be blank"], @parsed_response["errors"]["password"]
+  end
+
+  test "should not sign up -> no password confirmation" do
+    post user_registration_path, params: { user: { username: "test", password: "password" } }
+
+    assert_response :unprocessable_entity
+    assert_nothing_raised do
+      @parsed_response = JSON.parse(response.body)
+    end
+    assert_equal ["can't be blank"], @parsed_response["errors"]["password_confirmation"]
+  end
+
+  test "should not sign up -> password and password confirmation do not match" do
+    post user_registration_path, params: { user: { username: "test", password: "password", password_confirmation: "password1" } }
+
+    assert_response :unprocessable_entity
+    assert_nothing_raised do
+      @parsed_response = JSON.parse(response.body)
+    end
+
+    assert_equal ["doesn't match Password"], @parsed_response["errors"]["password_confirmation"]
+  end
+
+  test "should not sign up -> username already taken" do
+    post user_registration_path, params: { user: { username: users(:one).username, password: "password", password_confirmation: "password" } }
+
+    assert_response :unprocessable_entity
+    assert_nothing_raised do
+      @parsed_response = JSON.parse(response.body)
+    end
+
+    assert_equal ["has already been taken"], @parsed_response["errors"]["username"]
+  end
+
+  test "should not sign up -> no password and confirm" do
+    post user_registration_path, params: { user: { username: "test" } }
+
+    assert_response :unprocessable_entity
+    assert_nothing_raised do
+      @parsed_response = JSON.parse(response.body)
+    end
+
+    assert_equal ["can't be blank"], @parsed_response["errors"]["password"]
+    assert_equal ["can't be blank"], @parsed_response["errors"]["password_confirmation"]
+  end
+
+  # Sign in
+
+  test "should not sign in -> no user" do
+    sign_out users(:one)
+    post user_session_path, params: {}
+
+    assert_response :unauthorized
+    assert_nothing_raised do
+      @parsed_response = JSON.parse(response.body)
+    end
+    assert_equal "Invalid login credentials", @parsed_response["errors"]
+  end
+
+  test "should not sign in -> no username" do
+    sign_out users(:one)
+    post user_session_path, params: { user: { password: "password" } }
+
+    assert_response :unauthorized
+    assert_nothing_raised do
+      @parsed_response = JSON.parse(response.body)
+    end
+    assert_equal "Invalid login credentials", @parsed_response["errors"]
+  end
+
+  test "should not sign in -> no password" do
+    sign_out users(:one)
+    post user_session_path, params: { user: { username: users(:two).username } }
+
+    assert_response :unauthorized
+    assert_nothing_raised do
+      @parsed_response = JSON.parse(response.body)
+    end
+    assert_equal "Invalid login credentials", @parsed_response["errors"]
+  end
+
+  test "should not sign in -> wrong username" do
+    sign_out users(:one)
+    post user_session_path, params: { user: { username: "test", password: "password" } }
+
+    assert_response :unauthorized
+    assert_nothing_raised do
+      @parsed_response = JSON.parse(response.body)
+    end
+    assert_equal "Invalid login credentials", @parsed_response["errors"]
+  end
+
+  test "should not sign in -> wrong password" do
+    sign_out users(:one)
+    post user_session_path, params: { user: { username: users(:two).username, password: "password1" } }
+
+    assert_response :unauthorized
+    assert_nothing_raised do
+      @parsed_response = JSON.parse(response.body)
+    end
+    assert_equal "Invalid login credentials", @parsed_response["errors"]
+  end
+
+  # Sign out
+
+  test "should not sign out -> not signed in" do
+    sign_out users(:one)
+    delete destroy_user_session_path
+
+    assert_response :unauthorized
+    assert_nothing_raised do
+      @parsed_response = JSON.parse(response.body)
+    end
+    assert_equal "You need to sign in or sign up before continuing.", @parsed_response["errors"]
   end
 end
