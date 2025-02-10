49513d3834b11766bede5583b3fbe928c7bfde49
Aluzay <manonbro0105@gmail.com>
2025-02-10 14:31:51 -0500

    Refactoring x2

--- Rails/app/controllers/users/sessions_controller.rb
+++ Rails/app/controllers/users/sessions_controller.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # GET /users/sign_in
-  # def new
-  #   super
-  # end
-
   # POST /users/sign_in
   def create
     self.resource = warden.authenticate!(auth_options)
@@ -23,22 +16,11 @@ class Users::SessionsController < Devise::SessionsController
 
   protected
 
-  def auth_options
-    { scope: resource_name, recall: "#{controller_path}#new" }
-  end
+  # def auth_options
+  #   { scope: resource_name, recall: "#{controller_path}#new" }
+  # end
 
   def sign_in_params
     params.require(:user).permit(:username, :password)
   end
-
-  def check_session
-    if user_signed_in?
-      render json: { message: "User is already signed in", user: current_user }, status: 200
-    end
-  end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
