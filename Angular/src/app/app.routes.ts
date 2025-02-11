514328f37455b8a7ba3fb01fff76fcecb2dd67d6
HydroshieldMKII <114195376+HydroshieldMKII@users.noreply.github.com>
2025-02-11 08:50:30 -0500

    Added guards

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -3,8 +3,25 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 
+import { AuthenticationGuard } from './guards/authentication.guard';
+
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent }
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home',
+    canActivate: [AuthenticationGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login',
+    canActivate: [AuthenticationGuard]
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+    title: 'Sign up',
+    canActivate: [AuthenticationGuard]
+  },
 ];
---
+++ Angular/src/app/guards/authentication.guard.ts
@@ -0,0 +1,29 @@
+import { CanActivate, RouterStateSnapshot, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthenticationGuard implements CanActivate {
+    router: Router = inject(Router);
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+        const url: string = state.url;
+        const isLoggedIn = localStorage.getItem('printeasy.currentUser') !== 'null';
+
+        console.log('URL:', url);
+        console.log('Is logged in:', isLoggedIn);
+
+        if ((url.startsWith('/login') || url.startsWith('/signup')) && isLoggedIn) {
+            this.router.navigate(['/']);
+            return false;
+        }
+
+        if (!isLoggedIn && !(url.startsWith('/login') || url.startsWith('/signup'))) {
+            this.router.navigate(['/login']);
+            return false;
+        }
+
+        return true;
+    }
+}
