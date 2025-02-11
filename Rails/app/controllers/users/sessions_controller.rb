e1294f4712373f6dd9b7872cfde7cc70381ea3f3
Aluzay <manonbro0105@gmail.com>
2025-02-11 08:58:29 -0500

    Fix sign out

--- Rails/app/controllers/users/sessions_controller.rb
+++ Rails/app/controllers/users/sessions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
   # POST /users/sign_in
   def create
     self.resource = warden.authenticate!(auth_options)
@@ -10,7 +12,13 @@ class Users::SessionsController < Devise::SessionsController
 
   # DELETE /users/sign_out
   def destroy
-    sign_out(current_user)
+    sign_out(resource_name)
     render json: { errors: {} }, status: 200
+  end
+
+  private
+
+  def respond_to_on_destroy
+    render json: { errors: "You need to sign in or sign up before continuing." }, status: 401
   end
 end
