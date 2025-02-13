fdcb8408bf3b86442ae016a4397079dbddf39443
HydroshieldMKII <v.bureau@hotmail.com>
2025-02-13 12:03:06 -0500

    feat: update UserCredentialsModel to include optional countryId; modify signup component to utilize new model structure

--- Angular/src/app/models/user-credentials.model.ts
+++ Angular/src/app/models/user-credentials.model.ts
@@ -1,12 +1,13 @@
 export class UserCredentialsModel {
     readonly username: string
     readonly password: string
-    readonly confirmPassword: string
+    readonly confirmPassword?: string
+    readonly countryId?: number
 
-    constructor(username: string, password: string, confirmPassword?: string) {
+    constructor(username: string, password: string, confirmPassword?: string, countryId?: number) {
         this.username = username.toLowerCase()
         this.password = password
         this.confirmPassword = password
+        this.countryId = countryId
     }
-
 }
