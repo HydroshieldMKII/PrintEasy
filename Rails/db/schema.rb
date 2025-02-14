76ac8f140ac1aa627adb98fa24c8411ba1184e33
Aluzay <manonbro0105@gmail.com>
2025-02-14 11:16:17 -0500

    Added column is_admin for user

--- Rails/db/schema.rb
+++ Rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_13_164416) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_14_161128) do
   create_table "countries", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_13_164416) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.bigint "country_id", null: false
+    t.boolean "is_admin"
     t.index ["country_id"], name: "index_users_on_country_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
