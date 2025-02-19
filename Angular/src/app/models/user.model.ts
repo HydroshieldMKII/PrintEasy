1c523fa9501bfc6a28e12d6dfb8e152aa7553cde
Aluzay <manonbro0105@gmail.com>
2025-02-18 21:16:14 -0500

    Fixed merge

--- Angular/src/app/models/user.model.ts
+++ Angular/src/app/models/user.model.ts
@@ -2,10 +2,12 @@ export class UserModel {
     id: number;
     username: string;
     country: string;
+    createdAt?: Date;
 
-    constructor(id: number, username: string, country: string) {
+    constructor(id: number, username: string, country: string, createdAt?: Date) {
         this.id = id;
         this.username = username;
         this.country = country;
+        this.createdAt = createdAt;
     }
 }
