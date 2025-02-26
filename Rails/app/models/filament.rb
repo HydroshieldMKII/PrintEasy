19c93d1fc9a7b29da09bfd38c76f598dd4e2e848
HydroshieldMKII <v.bureau@hotmail.com>
2025-02-26 15:39:45 -0500

    fixed regex for seeding and brakeman

--- Rails/app/models/filament.rb
+++ Rails/app/models/filament.rb
@@ -1,3 +1,3 @@
 class Filament < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 60 }, format: { with: /\A[a-zA-Z]+\z/ }
+  validates :name, presence: true, length: { maximum: 60 }, format: { with: /\A[a-zA-Z\s\-]+\z/ }
 end
