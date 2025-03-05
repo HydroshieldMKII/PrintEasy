7759cde00387ae1f5dc3aee3f5dbb2f9e33504d4
HydroshieldMKII <v.bureau@hotmail.com>
2025-03-05 18:53:18 -0500

    rubocop fix

--- Rails/db/migrate/20250305232833_add_unique_to_offers_and_presets.rb
+++ Rails/db/migrate/20250305232833_add_unique_to_offers_and_presets.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class AddUniqueToOffersAndPresets < ActiveRecord::Migration[8.0]
   def change
-    add_index :offers, [:request_id, :printer_user_id, :color_id, :filament_id, :print_quality], unique: true
-    
-    add_index :presets, [:user_id, :filament_id, :color_id, :print_quality], unique: true
+    add_index :offers, %i[request_id printer_user_id color_id filament_id print_quality], unique: true
+
+    add_index :presets, %i[user_id filament_id color_id print_quality], unique: true
   end
 end
