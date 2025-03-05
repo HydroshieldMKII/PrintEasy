dc883b7d934a0d0c9f5627ea22c1ced107bd42db
Venorrak <jaudet199@gmail.com>
2025-03-05 07:31:39 -0500

    fixed home service

--- Angular/src/app/components/home/home.component.ts
+++ Angular/src/app/components/home/home.component.ts
@@ -4,9 +4,12 @@ import { MessageService } from 'primeng/api';
 import { ImportsModule } from '../../../imports';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { Router, RouterLink } from '@angular/router';
-import { HomeApi, HomeService } from '../../services/home.service';
+import { HomeApi, HomeModel, HomeService } from '../../services/home.service';
 import { ApiRequestService } from '../../services/api.service';
 import { ApiResponseModel } from '../../models/api-response.model';
+import { ContestModel } from '../../models/contest.model';
+import { RequestModel } from '../../models/request.model';
+import { SubmissionModel } from '../../models/submission.model';
 
 @Component({
   selector: 'app-home',
@@ -16,10 +19,18 @@ import { ApiResponseModel } from '../../models/api-response.model';
 })
 export class HomeComponent {
   readonly homeService = inject(HomeService);
+  contests : ContestModel[] = [];
+  requests : RequestModel[] = [];
+  submissions : SubmissionModel[] = [];
 
   constructor() {
-    this.homeService.getData().subscribe((response : HomeApi | ApiResponseModel) => {
+    this.homeService.getData().subscribe((response : HomeModel | ApiResponseModel) => {
       console.log("Home data: ", response);
+      if (response instanceof HomeModel) {
+        this.contests = response.contests;
+        this.requests = response.requests;
+        this.submissions = response.submissions;
+      }
     })
   }
 }
--- Angular/src/app/services/home.service.ts
+++ Angular/src/app/services/home.service.ts
@@ -7,9 +7,33 @@ import { RequestApi, RequestModel } from "../models/request.model";
 import { SubmissionAPI, SubmissionModel } from "../models/submission.model";
 
 export type HomeApi = {
-    contests: ContestAPI[],
-    requests: RequestApi[],
-    submissions: SubmissionAPI[]
+    contests: ContestAPI[];
+    requests: RequestApi[];
+    submissions: SubmissionAPI[];
+}
+
+export class HomeModel {
+    contests: ContestModel[];
+    requests: RequestModel[];
+    submissions: SubmissionModel[];
+
+    constructor(
+        contests: ContestModel[], 
+        requests: RequestModel[], 
+        submissions: SubmissionModel[]
+    ) {
+        this.contests = contests;
+        this.requests = requests;
+        this.submissions = submissions;
+    }
+
+    static fromAPI(api: HomeApi): HomeModel {
+        return new HomeModel(
+            api.contests.map((contest: ContestAPI) => ContestModel.fromApi(contest)),
+            api.requests.map((request: RequestApi) => RequestModel.fromAPI(request)),
+            api.submissions.map((submission: SubmissionAPI) => SubmissionModel.fromApi(submission))
+        );
+    }
 }
 
 @Injectable({
@@ -18,16 +42,12 @@ export type HomeApi = {
 export class HomeService {
     constructor(private api: ApiRequestService) { }
 
-    getData(): Observable<HomeApi | ApiResponseModel> {
+    getData(): Observable<HomeModel | ApiResponseModel> {
         return this.api.getRequest('/api/home').pipe(
             map((response : ApiResponseModel) => {
-                
+
                 if (response.status == 200) {
-                    return {
-                        contests: response.data.contests.map((contest: ContestAPI) => ContestModel.fromApi(contest)),
-                        requests: response.data.requests.map((request: RequestApi) => RequestModel.fromAPI(request)),
-                        submissions: response.data.submissions.map((submission: SubmissionAPI) => SubmissionModel.fromApi(submission))
-                    };
+                    return HomeModel.fromAPI(response.data as HomeApi);
                 }
                 return response;
             })
