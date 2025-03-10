c6b6ea84aba713f8a951e8fe3654cd77726210c2
Venorrak <jaudet199@gmail.com>
2025-03-10 10:57:37 -0400

    finished review form rework

--- Angular/src/app/components/order/order.component.ts
+++ Angular/src/app/components/order/order.component.ts
@@ -15,9 +15,9 @@ import { OrderService } from '../../services/order.service';
 import { OrderStatusService } from '../../services/order-status.service';
 import { ApiResponseModel } from '../../models/api-response.model';
 import { AuthService } from '../../services/authentication.service';
-import { ReviewService } from '../../services/review.service';
 import { ImageAttachmentModel } from '../../models/image-attachment.model';
 import { ReviewModel } from '../../models/review.model';
+import { ReviewFormComponent } from '../review-form/review-form.component';
 
 interface StatusChoice {
   name: string;
@@ -26,7 +26,7 @@ interface StatusChoice {
 
 @Component({
   selector: 'app-orders',
-  imports: [ImportsModule, DropdownModule, ReactiveFormsModule, StlModelViewerModule, TranslatePipe, RouterLink],
+  imports: [ImportsModule, DropdownModule, ReactiveFormsModule, StlModelViewerModule, TranslatePipe, RouterLink, ReviewFormComponent],
   templateUrl: './order.component.html',
   styleUrl: './order.component.css'
 })
@@ -36,7 +36,6 @@ export class OrderComponent {
   orderStatusService: OrderStatusService = inject(OrderStatusService);
   private readonly auth = inject(AuthService)
   messageService: MessageService = inject(MessageService);
-  reviewService: ReviewService = inject(ReviewService);
   translate: TranslateService = inject(TranslateService);
   router : Router = inject(Router);
 
@@ -92,17 +91,13 @@ export class OrderComponent {
   ShippedStatus: OrderStatusModel[] = [];
   ArrivedStatus: OrderStatusModel[] = [];
   CancelledStatus: OrderStatusModel[] = [];
-  reviewImageUrls: ImageAttachmentModel[] = [];
   canCancel: boolean = false;
   canArrive: boolean = false;
   consumer: boolean = false;
   formVisible: boolean = false;
   deleteStatusDialogVisible: boolean = false;
-  deleteReviewDialogVisible: boolean = false;
   isEdit: boolean = false;
-  isEditReview: boolean = false;
   orderStatusForm: FormGroup;
-  reviewForm: FormGroup;
   imageUrl: string = '';
   currentlySelectedOrderStatusId: number = -1
   tab : string = 'status';
@@ -136,13 +131,6 @@ export class OrderComponent {
       statusName: ['', [Validators.required, this.statusNameValidator.bind(this)]],
       comment: ['', [Validators.minLength(5), Validators.maxLength(200)]],
       image: [null, this.imageValidator.bind(this)]
-    });
-
-    this.reviewForm = this.fb.group({
-      title: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(30)]],
-      rating: [null, [Validators.min(0), Validators.max(5)]],
-      description: ['', [Validators.minLength(5), Validators.maxLength(200)]],
-      images: [[]]
     });
   }
 
@@ -217,13 +205,11 @@ export class OrderComponent {
     this.ArrivedStatus = [];
     this.CancelledStatus = [];
     this.statusActions = [];
-    this.reviewImageUrls = [];
     this.availableStatuses = [];
     this.canCancel = false;
     this.canArrive = false;
     this.consumer = false;
     this.isEdit = false;
-    this.isEditReview = false;
 
     const orderId = Number(this.route.snapshot.params['id']);
     this.orderService.getOrder(orderId).subscribe((response: ApiResponseModel | OrderModel) => {
@@ -234,94 +220,83 @@ export class OrderComponent {
           this.currentStatus = this.order.orderStatus[this.order.orderStatus.length - 1];
           console.log('Current status:', this.currentStatus);
           if (this.order.availableStatus.includes('Cancelled')) {
-        this.canCancel = true;
+            this.canCancel = true;
           }
           if (this.order.availableStatus.includes('Arrived')) {
-        this.canArrive = true;
+            this.canArrive = true;
           }
           if (this.order?.offer?.request?.user?.id == this.auth.currentUser?.id) {
-        this.consumer = true;
-          } else {
-        this.reviewForm.get('rating')?.disable();
-        this.reviewForm.get('title')?.disable();
-        this.reviewForm.get('description')?.disable();
+            this.consumer = true;
           }
           for (let status of this.order.orderStatus) {
-        this.sortStatus(status);
-          }
-          if (this.order.review) {
-        this.isEditReview = true;
-        this.reviewImageUrls = this.order.review.imageUrls;
-        this.reviewForm.patchValue({ title: this.order.review.title });
-        this.reviewForm.patchValue({ rating: this.order.review.rating });
-        this.reviewForm.patchValue({ description: this.order.review.description });
+            this.sortStatus(status);
           }
           for (let status of this.order.availableStatus) {
-        switch (status) {
-          case 'Accepted':
-            this.statusActions.push({
-          label: this.statusTranslations['Accepted'],
-          icon: 'pi pi-play',
-          command: () => {
-            this.ShowOrderStatusForm();
-            this.orderStatusForm.patchValue({ statusName: 'Accepted' });
-          }
-            });
-            break;
-          case 'Printing':
-            this.statusActions.push({
-          label: this.statusTranslations['Printing'],
-          icon: 'pi pi-print',
-          command: () => {
-            this.ShowOrderStatusForm();
-            this.orderStatusForm.patchValue({ statusName: 'Printing' });
-          }
-            });
-            break;
-          case 'Printed':
-            this.statusActions.push({
-          label: this.statusTranslations['Printed'],
-          icon: 'pi pi-check-circle',
-          command: () => {
-            this.ShowOrderStatusForm();
-            this.orderStatusForm.patchValue({ statusName: 'Printed' });
-          }
-            });
-            break;
-          case 'Shipped':
-            this.statusActions.push({
-          label: this.statusTranslations['Shipped'],
-          icon: 'pi pi-send',
-          command: () => {
-            this.ShowOrderStatusForm();
-            this.orderStatusForm.patchValue({ statusName: 'Shipped' });
-          }
-            });
-            break;
-          case 'Arrived':
-            this.statusActions.push({
-          label: this.statusTranslations['Arrived'],
-          icon: 'pi pi-home',
-          command: () => {
-            this.ShowOrderStatusForm();
-            this.orderStatusForm.patchValue({ statusName: 'Arrived' });
-          }
-            });
-            break;
-          case 'Cancelled':
-            this.statusActions.push({
-          label: this.statusTranslations['Cancelled'],
-          icon: 'pi pi-ban',
-          command: () => {
-            this.ShowOrderStatusForm();
-            this.orderStatusForm.patchValue({ statusName: 'Cancelled' });
-          }
-            });
-            break;
-        }
+            switch (status) {
+              case 'Accepted':
+                this.statusActions.push({
+                  label: this.statusTranslations['Accepted'],
+                  icon: 'pi pi-play',
+                  command: () => {
+                    this.ShowOrderStatusForm();
+                    this.orderStatusForm.patchValue({ statusName: 'Accepted' });
+                  }
+                });
+                break;
+              case 'Printing':
+                this.statusActions.push({
+                  label: this.statusTranslations['Printing'],
+                  icon: 'pi pi-print',
+                  command: () => {
+                    this.ShowOrderStatusForm();
+                    this.orderStatusForm.patchValue({ statusName: 'Printing' });
+                  }
+                });
+                break;
+              case 'Printed':
+                this.statusActions.push({
+                  label: this.statusTranslations['Printed'],
+                  icon: 'pi pi-check-circle',
+                  command: () => {
+                    this.ShowOrderStatusForm();
+                    this.orderStatusForm.patchValue({ statusName: 'Printed' });
+                  }
+                });
+                break;
+              case 'Shipped':
+                this.statusActions.push({
+                  label: this.statusTranslations['Shipped'],
+                  icon: 'pi pi-send',
+                  command: () => {
+                    this.ShowOrderStatusForm();
+                    this.orderStatusForm.patchValue({ statusName: 'Shipped' });
+                  }
+                });
+                break;
+              case 'Arrived':
+                this.statusActions.push({
+                  label: this.statusTranslations['Arrived'],
+                  icon: 'pi pi-home',
+                  command: () => {
+                    this.ShowOrderStatusForm();
+                    this.orderStatusForm.patchValue({ statusName: 'Arrived' });
+                  }
+                });
+                break;
+              case 'Cancelled':
+                this.statusActions.push({
+                  label: this.statusTranslations['Cancelled'],
+                  icon: 'pi pi-ban',
+                  command: () => {
+                    this.ShowOrderStatusForm();
+                    this.orderStatusForm.patchValue({ statusName: 'Cancelled' });
+                  }
+                });
+                break;
+            }
           }
           this.order.availableStatus.forEach((status: string) => {
-        this.availableStatuses.push({ name: this.statusTranslations[status], value: status });
+            this.availableStatuses.push({ name: this.statusTranslations[status], value: status });
           });
           console.log('Available statuses:', this.availableStatuses);
         }
@@ -338,19 +313,6 @@ export class OrderComponent {
     this.imageUrl = file["objectURL"].changingThisBreaksApplicationSecurity;
     this.orderStatusForm.patchValue({ image: file });
     console.log('Image:', file);
-  }
-
-  onReviewFileSelect(event: any) {
-    const files = event.files;
-    
-    for (let file of files) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        this.reviewImageUrls.push(new ImageAttachmentModel(null, reader.result as string, file ));
-      }
-      reader.readAsDataURL(file);
-    }
-    console.log('Review images:', this.reviewImageUrls);
   }
 
   ShowOrderStatusForm() : void {
@@ -416,67 +378,6 @@ export class OrderComponent {
     }
   }
 
-  onReviewSubmit() : void {
-    if (this.reviewForm.valid) {
-      console.log('Review data form:', this.reviewForm.value);
-
-      const reviewData = new FormData();
-      reviewData.append('review[title]', this.reviewForm.value.title);
-      reviewData.append('review[rating]', this.reviewForm.value.rating ?? 0);
-      reviewData.append('review[description]', this.reviewForm.value.description);
-      if (!this.isEditReview) {
-        reviewData.append('review[order_id]', this.order?.id.toString() || '');
-      }
-      if (this.reviewImageUrls.length > 0) {
-        this.reviewImageUrls.forEach((image : any) => {
-          reviewData.append('review[images][]', image.signedId ?? image.file);
-        });
-      }
-      console.log('Review data:', reviewData);
-      if (this.isEditReview) {
-        this.reviewService.updateReview(this.order?.review?.id || -1, reviewData).subscribe((response : ApiResponseModel | ReviewModel) => {
-          console.log('Review updated:', response);
-          if (response instanceof ReviewModel) {
-            this.clearReviewForm();
-            this.refreshOrder();
-            this.deleteReviewDialogVisible = false;
-            this.messageService.add({
-              severity: 'success',
-              summary: this.errors_type["summary_success"],
-              detail: this.errors_type["updated_success"]
-            });
-          }else{
-            this.messageService.add({
-              severity: 'error',
-              summary: this.errors_type["summary_error"],
-              detail: this.errors_type["updated_error"]
-            });
-          }
-        });
-      }else{
-        this.reviewService.createReview(reviewData).subscribe((response : ApiResponseModel | ReviewModel) => {
-          console.log('Review created:', response);
-          if (response instanceof ReviewModel) {
-            this.clearReviewForm();
-            this.refreshOrder();
-            this.deleteReviewDialogVisible = false;
-            this.messageService.add({
-              severity: 'success',
-              summary: this.errors_type["summary_success"],
-              detail: this.errors_type["created_success"]
-            });
-          }else{
-            this.messageService.add({
-              severity: 'error',
-              summary: this.errors_type["summary_error"],
-              detail: this.errors_type["created_error"]
-            });
-          }
-        });
-      }
-    }
-  }
-
   sortStatus(status: OrderStatusModel): void {
     switch (status.statusName) {
       case 'Accepted':
@@ -505,11 +406,6 @@ export class OrderComponent {
     this.orderStatusForm.reset();
     this.isEdit = false;
     this.imageUrl = '';
-  }
-
-  clearReviewForm() : void {
-    this.reviewForm.reset();
-    this.reviewImageUrls = [];
   }
 
   setStatusForm() : void {
@@ -544,40 +440,11 @@ export class OrderComponent {
           summary: this.errors_type["summary_error"],
           detail: this.errors_type["deleted_error"]
         });
-        this.clearReviewForm();
+        this.clearStatusForm();
       }
 
       
     });
-  }
-
-  DeleteReview() : void {
-    this.reviewService.deleteReview(this.order?.review?.id || -1).subscribe((response : ApiResponseModel | ReviewModel) => {
-      
-      if (response instanceof ReviewModel) {
-        console.log('Review deleted:', response);
-        this.refreshOrder();
-        this.deleteReviewDialogVisible = false;
-        this.messageService.add({
-          severity: 'success',
-          summary: this.errors_type["summary_success"],
-          detail: this.errors_type["deleted_success"]
-        });
-      }else{
-        this.messageService.add({
-          severity: 'error',
-          summary: this.errors_type["summary_error"],
-          detail: this.errors_type["deleted_error"]
-        });
-      }
-
-      
-
-    });
-  }
-
-  deleteImage(url: string) : void {
-    this.reviewImageUrls = this.reviewImageUrls.filter((image : ImageAttachmentModel) => image.url != url);
   }
 
   setSelectedOrderStatus(orderStatusId: number): void {
