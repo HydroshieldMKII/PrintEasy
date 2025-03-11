940f2ca00fda87cb91aae19c7189e4a8990aceec
Aluzay <manonbro0105@gmail.com>
2025-03-11 15:08:26 -0400

    WIP date frontend and sf work

--- Angular/src/app/components/leaderboard/leaderboard.component.ts
+++ Angular/src/app/components/leaderboard/leaderboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { ImportsModule } from '../../../imports';
 import { TranslatePipe } from '@ngx-translate/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { UserLeaderboardService } from '../../services/user-leaderboard.service';
 import { MessageService } from 'primeng/api';
@@ -30,6 +30,8 @@ export class LeaderboardComponent {
   currentSortCategory: string = '';
   minDate: Date = new Date(2000, 0, 1);
   maxDate = new Date();
+  startDate = new Date(2000, 0, 1);
+  endDate = new Date();
 
   filterOptions: SelectItem[] = [];
   sortOptions: SelectItem[] = [];
@@ -37,15 +39,32 @@ export class LeaderboardComponent {
   selectedSortOption: SelectItem | null = null;
   selectedFilterOption: SelectItem | null = null;
 
-  constructor() {
+  constructor(private activatedRoute: ActivatedRoute) {
     this.translateService.onLangChange.subscribe(() => {
       this.translateRefresh();
     });
     this.translateRefresh();
 
-    this.userLeaderboardService.getUserLeaderboard().subscribe((leaderboard: UserLeaderboardModel[]) => {
-      this.usersLeaderboard = leaderboard;
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.currentFilter = params['filter'] || '';
+      this.currentStartDate = params['startDate'] || '';
+      this.currentEndDate = params['endDate'] || '';
+      this.currentSortCategory = params['sortCategory'] || '';
+      this.currentSort = params['sort'] || '';
     });
+
+    if (this.currentEndDate) {
+      this.endDate = new Date(new Date(this.currentEndDate).setUTCHours(12, 0, 0, 0));
+      console.log(this.currentEndDate);
+    }
+
+    if (this.currentStartDate) {
+      this.startDate = new Date(new Date(this.currentStartDate).setUTCHours(12, 0, 0, 0));
+    }
+
+    this.selectedSortOption = this.sortOptions.find(option => option.value === `${this.currentSortCategory}-${this.currentSort}`) || null;
+
+    this.fetchLeaderboard();
   }
 
   translateRefresh() {
@@ -56,38 +75,33 @@ export class LeaderboardComponent {
       { label: this.translateService.instant('leaderboard.sort.participations-desc'), value: 'participations-desc' },
       { label: this.translateService.instant('leaderboard.sort.winrate-asc'), value: 'winrate-asc' },
       { label: this.translateService.instant('leaderboard.sort.winrate-desc'), value: 'winrate-desc' },
-      { label: this.translateService.instant('leaderboard.sort.total-likes-asc'), value: 'total-likes-asc' },
-      { label: this.translateService.instant('leaderboard.sort.total-likes-desc'), value: 'total-likes-desc' },
-      { label: this.translateService.instant('leaderboard.sort.submission-ratio-asc'), value: 'submission-ratio-asc' },
-      { label: this.translateService.instant('leaderboard.sort.submission-ratio-desc'), value: 'submission-ratio-desc' },
+      { label: this.translateService.instant('leaderboard.sort.total-likes-asc'), value: 'total_likes-asc' },
+      { label: this.translateService.instant('leaderboard.sort.total-likes-desc'), value: 'total_likes-desc' },
+      { label: this.translateService.instant('leaderboard.sort.submission-ratio-asc'), value: 'submission_rate-asc' },
+      { label: this.translateService.instant('leaderboard.sort.submission-ratio-desc'), value: 'submission_rate-desc' },
     ];
   }
 
-  onStartChange(event: any) {
-    const parsedDate = new Date(event).toISOString().slice(0, 10);
-    this.currentStartDate = new Date(parsedDate);
-
-    console.log(this.currentStartDate);
+  onStartChange(event: Date) {
+    this.currentStartDate = event;
 
     this.route.navigate([], {
-      queryParams: { startDate: this.currentStartDate || null },
+      queryParams: { startDate: this.currentStartDate.toISOString().split('T')[0] || null },
       queryParamsHandling: 'merge'
     });
 
-    // this.fetchContests();
+    this.fetchLeaderboard();
   }
 
-  onEndChange(event: any) {
-    this.currentEndDate = event.value;
-
-    console.log(event);
-
+  onEndChange(event: Date) {
+    this.currentEndDate = event;
+    
     this.route.navigate([], {
-      queryParams: { endDate: this.currentEndDate || null },
+      queryParams: { endDate: this.currentEndDate.toISOString().split('T')[0] || null },
       queryParamsHandling: 'merge'
     });
 
-    // this.fetchContests();
+    this.fetchLeaderboard();
   }
 
   onSortChange(event: any) {
@@ -104,14 +118,34 @@ export class LeaderboardComponent {
       queryParamsHandling: 'merge'
     });
 
-    // this.fetchContests();
+    this.fetchLeaderboard();
   }
 
   sf() {
     let sf_params = {};
 
-    if (this.currentFilter) {
-      sf_params = { ...sf_params, year: this.currentFilter };
+    if (this.currentStartDate) {
+      sf_params = { ...sf_params, start_date: new Date(this.currentStartDate).toISOString().split('T')[0] };	
     }
+
+    if (this.currentEndDate) {
+      sf_params = { ...sf_params, end_date: new Date(this.currentEndDate).toISOString().split('T')[0] };	
+    }
+
+    if (this.currentSortCategory) {
+      sf_params = { ...sf_params, category: this.currentSortCategory };	
+    }
+
+    if (this.currentSort) {
+      sf_params = { ...sf_params, direction: this.currentSort };	
+    }
+    console.log(sf_params);
+    return sf_params;
+  }
+
+  fetchLeaderboard() {
+    this.userLeaderboardService.getUserLeaderboard(this.sf()).subscribe((leaderboard: UserLeaderboardModel[]) => {
+      this.usersLeaderboard = leaderboard;
+    });
   }
 }
--- Rails/app/models/user.rb
+++ Rails/app/models/user.rb
@@ -28,13 +28,13 @@ class User < ApplicationRecord
   # the not exists subquery is to find submissions who have the same amount of likes but were created before the current submission or more likes than the current
   # not exists if true means that there is no other submissions that have the same amount or like or more
   # not exists if false means that there is another submission that has the same amount of likes or more
-  def self.stats(order_by:, direction:, start_date:, end_date:)
-    valid_order_columns = ['wins_count', 'submissions_participation_rate', 'contests_count', 'likes_received_count', 'winrate']
-   
-    order_by = valid_order_columns.include?(order_by) ? order_by : 'wins_count'
-   
-    direction = ['ASC', 'DESC'].include?(direction) ? direction : 'DESC'
-    
+  def self.stats(category:, direction:, start_date:, end_date:)
+    valid_order_columns = ['wins_count', 'submission_rate', 'participations', 'total_likes', 'winrate']
+
+    category = valid_order_columns.include?(category) ? category : 'wins_count'
+  
+    direction = ['asc', 'desc'].include?(direction) ? direction : 'DESC'
+
     start_date ||= Date.new(2000, 1, 1).strftime('%Y-%m-%d')
     end_date ||= Date.today.strftime('%Y-%m-%d')
 
@@ -74,7 +74,7 @@ class User < ApplicationRecord
         GROUP BY submissions.user_id
       ),
       submission_ratio AS (
-        SELECT AVG(submission_ratio) AS submissions_participation_rate, ratios.user_id
+        SELECT AVG(submission_ratio) AS submission_rate, ratios.user_id
         FROM (
           SELECT contests.id, contests.submission_limit,  
                  (COUNT(submissions.id) / contests.submission_limit) AS submission_ratio,
@@ -88,16 +88,16 @@ class User < ApplicationRecord
         ) AS ratios
         GROUP BY ratios.user_id
       ),
-      contests_count AS (
-        SELECT COUNT(DISTINCT contests.id) AS contests_count, submissions.user_id
+      participations AS (
+        SELECT COUNT(DISTINCT contests.id) AS participations, submissions.user_id
         FROM contests
         LEFT JOIN submissions ON contests.id = submissions.contest_id
         WHERE contests.deleted_at IS NULL
         #{year_condition}
         GROUP BY submissions.user_id
       ),
-      likes_received_count AS (
-        SELECT COUNT(*) AS likes_received_count, submissions.user_id
+      total_likes AS (
+        SELECT COUNT(*) AS total_likes, submissions.user_id
         FROM likes
         LEFT JOIN submissions ON likes.submission_id = submissions.id
         INNER JOIN contests ON contests.id = submissions.contest_id
@@ -109,31 +109,30 @@ class User < ApplicationRecord
         SELECT
           contests_won.user_id,
           CASE
-            WHEN contests_count.contests_count = 0 THEN 0
-            ELSE ROUND((contests_won.wins_count * 1.0 / contests_count.contests_count) * 100, 2)
+            WHEN participations.participations = 0 THEN 0
+            ELSE ROUND((contests_won.wins_count * 1.0 / participations.participations) * 100, 2)
           END AS winrate
         FROM contests_won
-        LEFT JOIN contests_count ON contests_won.user_id = contests_count.user_id
+        LEFT JOIN participations ON contests_won.user_id = participations.user_id
       )
       SELECT
         users.username AS username,
         COALESCE(contests_won.wins_count, 0) AS wins_count,
-        COALESCE(submission_ratio.submissions_participation_rate * 100, 0) AS submissions_participation_rate,
-        COALESCE(contests_count.contests_count, 0) AS contests_count,
-        COALESCE(likes_received_count.likes_received_count, 0) AS likes_received_count,
+        COALESCE(submission_ratio.submission_rate * 100, 0) AS submission_rate,
+        COALESCE(participations.participations, 0) AS participations,
+        COALESCE(total_likes.total_likes, 0) AS total_likes,
         COALESCE(winrate.winrate, 0) AS winrate
       FROM users
       LEFT JOIN contests_won ON users.id = contests_won.user_id
       LEFT JOIN submission_ratio ON users.id = submission_ratio.user_id
-      LEFT JOIN contests_count ON users.id = contests_count.user_id
-      LEFT JOIN likes_received_count ON users.id = likes_received_count.user_id
+      LEFT JOIN participations ON users.id = participations.user_id
+      LEFT JOIN total_likes ON users.id = total_likes.user_id
       LEFT JOIN winrate ON users.id = winrate.user_id
-      ORDER BY #{order_by} #{direction};
+      ORDER BY #{category} #{direction};
     SQL
     
     results = ActiveRecord::Base.connection.execute(sql)
     
-    # Convertir les résultats en un tableau de hachages pour chaque utilisateur
     result_hashes = results.map do |result|
       {
         username: result[0],
@@ -144,7 +143,7 @@ class User < ApplicationRecord
         winrate: result[5].to_f.round(2)
       }
     end
-    
+
     result_hashes
   end
 
